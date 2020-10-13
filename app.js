01b2950d9c21c4888bfcd4f2ec067c0991b1f3ca
gk-svg <gk32239@gmail.com>
2020-10-13 11:42:16 +0530

    products added dynamically from products.json file

--- app.js
+++ app.js
@@ -13,11 +13,61 @@ let cart = []
 
 //getting products
 
-class Products {}
+class Products {
+    async getProducts() {
+        try {
+            let result = await fetch('products.json')
+            let data = await result.json()
+            let products = data.items
+            products = products.map(item => {
+                const {
+                    title,
+                    price
+                } = item.fields
+                const {
+                    id
+                } = item.sys
+                const 
+                    image
+                 = item.fields.image.fields.file.url
+                return {
+                    title,
+                    price,
+                    id,
+                    image
+                }
+            })
+            return products
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
 
 // display products
 
-class UI {}
+class UI {
+    displayProduct(products) {
+        console.log(products)
+        let results = ''
+        products.forEach(product => {
+            results += `
+            <article class="product">
+            <div class="img-container">
+                <img src=${product.image} alt="product" 
+                class="product-img">
+                <button class="bag-btn" data-id=${product.id}>
+                    <i class="fas fa-shopping-cart"></i>
+                    add to cart
+                </button>
+            </div>
+            <h3>${product.title}</h3>
+            <h4>$${product.price}</h4>
+        </article>`
+        });
+        productsDOM.innerHTML=results
+    }
+}
 
 // local storage
 
@@ -26,4 +76,8 @@ class Storage {}
 document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI()
     const products = new Products()
+    // get all product
+    products.getProducts().then(products =>
+        ui.displayProduct(products))
+
 })
